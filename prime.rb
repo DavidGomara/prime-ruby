24953174a4df8645d1baa8a007a57130b8d9854e
David Gomara <davidgomara7@gmail.com>
2020-11-17 07:43:31 +0000

    commit tree d0ea292f451a2514aadafbec3c3d9cab369abb4e

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+    new_array = []
+    range = (2..num - 1).to_a
+    if  num < 2
+      new_array << false
+    end
+    range.each do |number|
+        if num % number == 0
+            new_array << false
+        else
+            new_array << true
+        end
+    end
+    new_array.all?(true)
+end
+
+	
